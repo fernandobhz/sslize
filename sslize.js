8d1313e1f625edf75aebfe5bf0140b7eaba245ad
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2017-11-09 19:08:36 -0200

    versao beta

--- sslize.js
+++ sslize.js
@@ -1,17 +1,31 @@
 #! /usr/bin/env node
 
-var proxy = require('redbird')({port: 80, xfwd: false});
+var proxy = require('winredbird')({
+	port: 80
+	, xfwd: false
+	, 	ssl: {
+			port: 443
+			, http2: true
+		}
+	, letsencrypt: {
+		path: __dirname + '/certs/'
+		, port: 80
+	}
+});
+
+var opts = {
+	ssl: {
+		letsencrypt: {
+			path: __dirname + '/certs'
+			, email: 'fernandobhz@gmail.com'
+			, production: false
+		}
+	}
+}
 
 proxy.notFound( function(req, res){
-	proxy.register(req.headers.host, `http://${req.headers.host}:8080`, {
-		ssl: {
-			letsencrypt: {
-				email: 'fernandobhz@gmail.com', // Domain owner/admin email
-				production: false
-			}
-		}
-	});
-
+	proxy.register(req.headers.host, `http://${req.headers.host}:8080`, opts);
 	res.writeHead(302, {'Location': req.url});
 	res.end();
 });
+
