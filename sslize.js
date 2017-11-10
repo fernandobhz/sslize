26b9ca281a6ca7d357015114a5366d301022a414
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2017-11-10 14:12:02 -0200

    melhorias

--- sslize.js
+++ sslize.js
@@ -54,19 +54,9 @@ Registering ${host}
 			registered.push(host);
 			register(req, res);
 		} else {
-console.log(`
-
-Redirection to https ${req.headers.host}${req.url}
-
-`);
-			res.write(`
-				<script>
-					location = 'https://' + location.hostname + location.pathname + location.hash;
-				</script>
-			`);
-			res.end();
+			//that code will never be hit
+			console.log('\n\n\n\n\n\n\n\n\n\n that code never shoud be hit \n\n\n\n\n\n\n\');
 		}
-
 	});
 });
 
