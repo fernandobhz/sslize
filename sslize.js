d647bad13af5bedc0e9fb1f40053a1e93e774228
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2017-11-10 19:01:14 -0200

    melhorias

--- sslize.js
+++ sslize.js
@@ -20,44 +20,41 @@ var email = process.argv[2];
 var destination = process.argv[3];
 
 if ( process.argv[4] == 'force' ) {
-	var production = true;
+	var server = greenlock.productionServerUrl;
 	var force = true;
 } else if ( process.argv[4] == 'true') {
-	var production = true;
+	var server = greenlock.productionServerUrl;
 	var force = false;
 } else {
-	var production = false;
+	var server = greenlock.stagingServerUrl;
 	var force = false;
 }
 
 
 var proxy = httpProxy.createProxyServer({xfwd: false});
-var leStaging = greenlock.create({ server: greenlock.stagingServerUrl });
-var leProduction = greenlock.create({ server: greenlock.productionServerUrl });
-var leStagingMiddleware = leStaging.middleware();
-var leProductionMiddleware = leProduction.middleware();
-
+var le = greenlock.create({ server: server });
+var leMiddleware = le.middleware();
 
 http.createServer(async function(req, res) {
+	var host = req.headers.host;
 	console.log(`Received request ${req.headers.host}${req.url}`);
 
-	leProductionMiddleware(req, res, function() {
+	leMiddleware(req, res, function() {
 		httpHttps(req, res);
 	});
 
 }).listen(80);
 
+var polyglot;
 
 var httpHttps = function(req, res) {
-	var host = req.headers.host;
-	
 	if ( ! host ) {
 		var errMessage = `HOST IS NOT VALID: '${host}'`;
 		console.log(errMessage);
 		res.statusCode = 500;
 		res.write(errMessage);
 		res.end();
-	} else if ( ! isNaN(host[0]) ) {
+	} else if ( skip.includes(host) ) {
 		console.log(`IP: ${req.headers.host}${req.url}`);
 		proxy.web(req, res, { target: destination });
 	} else if ( skip.includes(host) ) {
@@ -76,27 +73,31 @@ var httpHttps = function(req, res) {
 		registered.unshift(host);
 
 		console.log(`ASK-LETSENCRYPT ${registered}`);
-		leStaging.register({"domains": registered, "email": email, "agreeTos": true}).then(function(certs) {
-			console.log('Successfully TESTED ssls certs');
+		le.register({"domains": registered, "email": email, "agreeTos": true}).then(function(certs) {
+			console.log('Successfully registered ssls certs');
 
-			if ( ! production ) {
-				https(req, res, certs);
+			if ( polyglot ) polyglot.close();
+
+			polyglot = httpolyglot.createServer({
+			  key: certs.privkey
+			  , cert: certs.cert
+			  , ca: certs.chain
+			}, function(req, res) {
+				if ( ! registered.includes(req.headers.host) ) {
+					httpHttps(req, res);
+				} else {
+					proxy.web(req, res, { target: destination });
+				}
+			});
+			polyglot.listen(443);
+
+			if ( force ) {
+				res.writeHead(302, {'Location': `https://${req.headers.host}${req.url}`});
+				res.end();
 			} else {
-				leProduction.register({"domains": registered, "email": email, "agreeTos": true}).then(function(certs) {
-					console.log('Successfully REGISTERED ssls certs');
-				
-					https(req, res, certs);
-				}, function(err) {
-					console.log('Error REGISTERING ssl cert');
-					console.log(err);
-
-					res.statusCode = 500;
-					res.write(err.message);
-					res.end();
-				});
-			}
+				proxy.web(req, res, { target: destination });
+			}		
 		}, function(err) {
-			console.log('Error TESTING ssl cert');
 			console.log(err);
 
 			res.statusCode = 500;
@@ -106,28 +107,3 @@ var httpHttps = function(req, res) {
 	}
 }
 
-var polyglot;
-
-function https(req, res, certs) {
-	if ( polyglot ) polyglot.close();
-
-	polyglot = httpolyglot.createServer({
-	  key: certs.privkey
-	  , cert: certs.cert
-	  , ca: certs.chain
-	}, function(req, res) {
-		if ( ! registered.includes(req.headers.host) ) {
-			httpHttps(req, res);
-		} else {
-			proxy.web(req, res, { target: destination });
-		}
-	});
-	polyglot.listen(443);
-
-	if ( force ) {
-		res.writeHead(302, {'Location': `https://${req.headers.host}${req.url}`});
-		res.end();
-	} else {
-		proxy.web(req, res, { target: destination });
-	}
-}
