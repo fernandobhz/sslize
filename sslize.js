b58c57881186533154774b44fb292cac28b6ce69
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2024-03-06 21:28:03 -0300

    2.0.0

--- sslize.js
+++ sslize.js
@@ -12,7 +12,9 @@ if (process.argv.length < 5) {
 // REQUIRES
 const home = require('home')();
 const httpProxy = require('http-proxy');
-const greenlock = require('greenlock').create({ packageAgent: 'sslize/1.3.55' });
+const greenlock = require('greenlock-express').create({
+    packageAgent: 'sslize/1.3.55'
+});
 const request = require('request');
 const https = require('https');
 const path = require('path');
